c1686f565caa5f77c320de7ef86af7b5fe353a03
Pace Willisson <pace@alum.mit.edu>
2009-08-02 11:48:20 -0400

    remove poll loop

--- img.c
+++ img.c
@@ -2,44 +2,36 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <memory.h>
+#include <fcntl.h>
 #include <sys/time.h>
 #include <sys/inotify.h>
 
 #include <gtk/gtk.h>
+#include <gdk/gdkkeysyms.h>
 
 char *filename;
 int inotify_fd;
 
+int auto_raise = 1;
+
 void
 usage (void)
 {
-	fprintf (stderr, "usage: img file\n");
+	fprintf (stderr, "usage: img [-a 0|1] file\n");
 	exit (1);
 }
 
-
 GtkWidget *window;
 GdkPixbuf *pixbuf;
-GdkGC *gc;
 
 gboolean
 expose_event ()
 {
 	if (pixbuf) {
-		gdk_draw_rectangle (window->window,
-				    gc, TRUE, 0, 0,
-				    window->allocation.width,
-				    window->allocation.height);
-
-		gdk_pixbuf_render_to_drawable (pixbuf,
-					       GDK_WINDOW (window->window),
-					       gc,
-					       0, 0,
-					       0, 0,
-					       gdk_pixbuf_get_width (pixbuf),
-					       gdk_pixbuf_get_height (pixbuf),
-					       GDK_RGB_DITHER_NORMAL,
-					       0, 0);
+		gdk_draw_pixbuf (window->window, NULL, pixbuf,
+				 0, 0, 0, 0,
+				 -1, -1,
+				 GDK_RGB_DITHER_NORMAL, 0, 0);
 	}
 	return (TRUE);
 }
@@ -63,6 +55,11 @@ read_image (void)
 			   gdk_pixbuf_get_width (pixbuf),
 			   gdk_pixbuf_get_height (pixbuf));
 
+	gtk_widget_queue_draw (window);
+
+	if (auto_raise && window->window)
+		gdk_window_raise (GDK_WINDOW (window->window));
+	
 	return (0);
 }
 
@@ -70,88 +67,120 @@ gboolean
 key_press_event (GtkWidget *widget, GdkEventKey *ev, gpointer data)
 {
 	switch (ev->keyval) {
-	case 'r':
-		read_image ();
-		gtk_widget_queue_draw_area (window, 0, 0,
-					    window->allocation.width,
-					    window->allocation.height);
-		break;
-	case 'q':
-	case 'c':
-	case 'w':
-		exit (0);
+	case 'q': /* covers q or ALT-q */
+	case 'c': /* covers CTL-c */
+	case 'w': /* covers ALT-w */
+	case GDK_Escape:
+		gtk_main_quit ();
 	}
 	return (TRUE);
 }
 
-double
-get_secs (void)
-{
-	struct timeval tv;
-	gettimeofday (&tv, NULL);
-	return (tv.tv_sec + tv.tv_usec / 1e6);
-}
+struct monitor_file {
+	struct monitor_file *next;
+	char *dirname;
+	char *filename;
+	void (*func)(struct monitor_file *);
+};
 
-enum {
-	mod_idle,
-	mod_pending,
-	mod_deleted,
-} mod_state;
+struct monitor_file *monitor_files;
 
-double mod_secs;
-
-gboolean
-tick (gpointer data)
-{
-	switch (mod_state) {
-	case mod_idle:
-		break;
-	case mod_pending:
-		if (get_secs () - mod_secs < .5)
-			break;
-
-		read_image ();
-
-		gtk_widget_queue_draw_area (window, 0, 0,
-					    window->allocation.width,
-					    window->allocation.height);
-		mod_state = mod_idle;
-		break;
-
-	case mod_deleted:
-		if (access (filename, R_OK) < 0)
-			break;
-
-		if (inotify_add_watch (inotify_fd, filename,
-				       IN_MODIFY | IN_DELETE_SELF) < 0) {
-			fprintf (stderr, "error doing inotify_add_watch\n");
-			exit (1);
-		}
-		mod_state = mod_pending;
-		break;
-	}
-	return (TRUE);
-}
+int inotify_fd = -1;
 
 gboolean
 inotify_handler (GIOChannel *source, GIOCondition condition, gpointer data)
 {
 	char buf[10000];
 	struct inotify_event ev;
+	ssize_t avail, offset;
+	struct monitor_file *mp;
 
-	if (read (inotify_fd, buf, sizeof buf) < 0)
-		return (TRUE);
+	while ((avail = read (inotify_fd, buf, sizeof buf)) > 0) {
+		offset = 0;
+		while (offset + sizeof ev <= avail) {
+			memcpy (&ev, buf + offset, sizeof ev);
+			filename = buf + offset + sizeof ev;
 
-	memcpy (&ev, buf, sizeof ev);
-
-	if (ev.mask & IN_DELETE_SELF) {
-		mod_state = mod_deleted;
-	} else {
-		mod_state = mod_pending;
-		mod_secs = get_secs ();
+			if (ev.mask & (IN_CLOSE_WRITE | IN_MOVED_TO)) {
+				for (mp = monitor_files; mp; mp = mp->next) {
+					if (strcmp (mp->filename,
+						    filename) == 0) {
+						(*mp->func)(mp);
+					}
+				}
+			}
+			offset += sizeof ev + ev.len;
+		}
 	}
 
 	return (TRUE);
+}
+
+struct monitor_file *
+monitor_file (char *filename_arg,
+	      void (*file_changed_func)(struct monitor_file *))
+{
+	char *dirname;
+	int total_len, dirname_len, filename_len, idx;
+	struct monitor_file *mp;
+	void *mpbuf;
+
+	if ((total_len = strlen (filename_arg)) == 0)
+		return (NULL);
+
+	idx = total_len;
+	while (idx > 0 && filename_arg[idx-1] != '/')
+		idx--;
+
+	filename = filename_arg + idx;
+	filename_len = total_len - idx;
+
+	while (idx > 0 && filename_arg[idx-1] == '/')
+		idx--;
+
+	if (idx == 0) {
+		dirname = (filename_arg[0] == '/') ? "/" : ".";
+		dirname_len = 1;
+	} else {
+		dirname = filename_arg;
+		dirname_len = idx;
+	}
+		
+	if ((mpbuf = malloc (sizeof *mp
+			     + filename_len + 1
+			     + dirname_len + 1)) == NULL) {
+		return (NULL);
+	}
+
+	mp = mpbuf;
+	memset (mp, 0, sizeof *mp);
+
+	mp->filename = mpbuf + sizeof *mp;
+	strncpy (mp->filename, filename, filename_len);
+	mp->filename[filename_len] = 0;
+
+	mp->dirname = mp->filename + filename_len + 1;
+	strncpy (mp->dirname, dirname, dirname_len);
+	mp->dirname[dirname_len] = 0;
+	
+	if (inotify_add_watch (inotify_fd, mp->dirname, 
+			       IN_CLOSE_WRITE | IN_MOVED_TO) < 0) {
+		free (mp);
+		return (NULL);
+	}
+
+	mp->func = file_changed_func;
+
+	mp->next = monitor_files;
+	monitor_files = mp;
+
+	return (mp);
+}
+
+void
+monitor_file_func (struct monitor_file *mp)
+{
+	read_image ();
 }
 
 int
@@ -161,8 +190,11 @@ main (int argc, char **argv)
 
 	gtk_init (&argc, &argv);
 
-	while ((c = getopt (argc, argv, "")) != EOF) {
+	while ((c = getopt (argc, argv, "a:")) != EOF) {
 		switch (c) {
+		case 'a':
+			auto_raise = atoi (optarg);
+			break;
 		default:
 			usage ();
 		}
@@ -179,16 +211,6 @@ main (int argc, char **argv)
 	window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
 	gtk_window_set_title (GTK_WINDOW (window), filename);
 
-	if (read_image () < 0) {
-		fprintf (stderr, "can't open %s\n", filename);
-		exit (1);
-	}
-
-	gtk_window_set_default_size (GTK_WINDOW (window), 
-				     gdk_pixbuf_get_width (pixbuf),
-				     gdk_pixbuf_get_height (pixbuf));
-
-
 	g_signal_connect (G_OBJECT (window), "delete_event",
 			  G_CALLBACK (gtk_main_quit), NULL);
 
@@ -202,25 +224,28 @@ main (int argc, char **argv)
 			  G_CALLBACK (key_press_event), NULL);
 
 	gtk_widget_show (window);
-	gc = gdk_gc_new (window->window);
 
-
-	if ((inotify_fd = inotify_init1 (IN_NONBLOCK)) < 0) {
+	if ((inotify_fd = inotify_init1 (IN_NONBLOCK | IN_CLOEXEC)) < 0) {
 		fprintf (stderr, "error doing inotify_init\n");
 		exit (1);
 	}
 
-	if (inotify_add_watch (inotify_fd, filename,
-			       IN_MODIFY | IN_DELETE_SELF) < 0) {
-		fprintf (stderr, "error doing inotify_add_watch\n");
-		exit (1);
-	}
-	
-	g_timeout_add (100, tick, NULL);
+	fcntl (inotify_fd, F_SETFL, O_NONBLOCK);
 
 	g_io_add_watch (g_io_channel_unix_new (inotify_fd), G_IO_IN,
 			inotify_handler, NULL);
 
+	if (monitor_file (filename, monitor_file_func) == NULL) {
+		fprintf (stderr, "error setting up monitor_file\n");
+		exit (1);
+	}
+
+	if (read_image () < 0) {
+		fprintf (stderr, "can't open %s\n", filename);
+		exit (1);
+	}
+
 	gtk_main ();
 	return (0);
 }
+
